6de187e14c57490f181826b2364ae4a1f59dd1e4
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-05-08 18:23:52 +0200

    Rename EuropeanArticleNumber attribute to EAN

--- Core/Model/Product.cs
+++ Core/Model/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Varasto.Core.Model
 {
@@ -9,7 +10,8 @@ namespace Varasto.Core.Model
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int EuropeanArticleNumber { get; set; }
+        
+        public int EAN { get; set; }
         
         public Category Category { get; set; }
         public List<Label> Labels { get; set; }
