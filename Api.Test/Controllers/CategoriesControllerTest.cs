0484a3aaf9448b1ed50ef22ccff284876fbd280d
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-05-16 19:01:42 +0200

    Define test methods to validate GET functionalities

--- Api.Test/Controllers/CategoriesControllerTest.cs
+++ Api.Test/Controllers/CategoriesControllerTest.cs
@@ -1,17 +1,33 @@
-﻿using Varasto.Api.Controllers;
+﻿using System.Linq;
+using Varasto.Api.Controllers;
 using Varasto.Core;
 using Varasto.Core.Database;
+using Varasto.Core.Test.Database;
+using Xunit;
 
 namespace Varasto.Api.Test.Controllers
 {
     public class CategoriesControllerTest
     {
         private readonly CategoriesController _categoriesController;
+        private readonly DatabaseContext _databaseContext;
 
         public CategoriesControllerTest()
         {
-            var databaseContext = new DatabaseContext(Globals.DbContextInMemoryConfig);
-            _categoriesController = new CategoriesController(databaseContext);
+            _databaseContext = new DatabaseContext(Globals.DbContextInMemoryConfig);
+            _categoriesController = new CategoriesController(_databaseContext);
+            
+            if (!_databaseContext.Products.Any())
+            {
+                DatabaseSeeder.Seed(_databaseContext);
+            }
+        }
+        
+        [Fact]
+        public void GetCategories_Test()
+        {
+            var categories = _categoriesController.GetCategories();
+            Assert.Equal(_databaseContext.Categories.Count(), categories.Count());
         }
     }
 }
--- Api.Test/Controllers/ProductsControllerTest.cs
+++ Api.Test/Controllers/ProductsControllerTest.cs
@@ -1,17 +1,40 @@
 ﻿using Varasto.Api.Controllers;
 using Varasto.Core;
 using Varasto.Core.Database;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Varasto.Core.Database;
+using Varasto.Core.Model;
+using Varasto.Core.Test.Database;
+using Xunit;
 
 namespace Varasto.Api.Test.Controllers
 {
     public class ProductsControllerTest
     {
         private readonly ProductsController _productsController;
+        private readonly DatabaseContext _databaseContext;
 
         public ProductsControllerTest()
         {
-            var databaseContext = new DatabaseContext(Globals.DbContextInMemoryConfig);
-            _productsController = new ProductsController(databaseContext);
+            _databaseContext = new DatabaseContext(Globals.DbContextInMemoryConfig);
+            _productsController = new ProductsController(_databaseContext);
+            
+            if (!_databaseContext.Products.Any())
+            {
+                DatabaseSeeder.Seed(_databaseContext);
+            }
+        }
+
+        [Fact]
+        public void GetProducts_Test()
+        {
+            var products = _productsController.GetProducts();
+            Assert.Equal(_databaseContext.Products.Count(), products.Count());
         }
     }
 }
