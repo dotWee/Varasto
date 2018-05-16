c4b31f4a94c9a78c82946bda83c9f464f2e803ec
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-05-16 18:35:10 +0200

    Defined test classes for api controller

---
+++ Api.Test/Controllers/CategoriesControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Varasto.Api.Controllers;
+using Varasto.Core;
+using Varasto.Core.Database;
+
+namespace Varasto.Api.Test.Controllers
+{
+    public class CategoriesControllerTest
+    {
+        private readonly CategoriesController _categoriesController;
+
+        public CategoriesControllerTest()
+        {
+            var databaseContext = new DatabaseContext(Globals.DbContextInMemoryConfig);
+            _categoriesController = new CategoriesController(databaseContext);
+        }
+    }
+}
---
+++ Api.Test/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using Varasto.Api.Controllers;
+using Varasto.Core;
+using Varasto.Core.Database;
+
+namespace Varasto.Api.Test.Controllers
+{
+    public class ProductsControllerTest
+    {
+        private readonly ProductsController _productsController;
+
+        public ProductsControllerTest()
+        {
+            var databaseContext = new DatabaseContext(Globals.DbContextInMemoryConfig);
+            _productsController = new ProductsController(databaseContext);
+        }
+    }
+}
