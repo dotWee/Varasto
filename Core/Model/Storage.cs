a1bc6b7be2d21c855c2f64b329c0bea82d46e164
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-05-18 14:34:23 +0200

    Implement storage model class

---
+++ Core/Model/Storage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Varasto.Core.Model
+{
+    public class Storage
+    {
+        [Key]
+        [DisplayName("ID")]
+        public int StorageId { get; set; }
+        public string Name { get; set; }
+        
+        [DisplayName("Produkte")]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
